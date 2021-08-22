8503bc66fcc2ea7eb2b9336159e4c1b02b09cb02
vavarine <evailson.m.barbosa@gmail.com>
2021-08-21 22:37:05 -0300

    fix(login): layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,19 @@
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
+import LandingPage from './LandingPage';
 
 function Login() {
-  return (
-    <>
-      <Head>
-        <title>AchaPet</title>
-      </Head>
-      <h1>LandingPage</h1>
-      <Link href="/Login">
-        <a>Login</a>
-      </Link>
-      <br />
-      <Link href="/sign-in">
-        <a>Cadastrar-se</a>
-      </Link>
-    </>
-  );
+  return <LandingPage />;
 }
 
-export const getServerSideProps: GetServerSideProps = async ctx => {
-  return {
-    redirect: {
-      destination: '/Login',
-      permanent: false,
-    },
-  };
-};
+// export const getServerSideProps: GetServerSideProps = async ctx => {
+//   // return {
+//   //   redirect: {
+//   //     destination: '/Login',
+//   //     permanent: false,
+//   //   },
+//   // };
+// };
 
 export default Login;
