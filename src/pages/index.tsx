ed9fca5820af3368380e19b2135be406c61f219a
vavarine <evailson.m.barbosa@gmail.com>
2021-08-22 12:05:43 -0300

    fix(login): layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
-import LandingPage from './LandingPage';
+import LandingPage from './landing';
 
 function Login() {
   return <LandingPage />;
