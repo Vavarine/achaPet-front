b7844b4ee36fb84979984c78a844efc6e67a4605
vavarine <evailson.m.barbosa@gmail.com>
2021-10-19 21:53:46 -0300

    Adding geolocation

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,38 +1,63 @@
-import { MapContainer, TileLayer } from 'react-leaflet';
+import React, { useState, useEffect } from 'react';
+import { icon } from 'leaflet';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import Doggo from '../../assets/doggo.svg';
 import 'leaflet/dist/leaflet.css';
-import React from 'react';
-
-// const [lat, useLat] = useState(51.505);
-// const [lon, useLon] = useState(-0.09);
-
-// const GetPosition = () => {
-//   navigator.geolocation.getCurrentPosition(
-//     sucess => {
-//       console.log('sucess :>> ', sucess);
-//       useLat(sucess.coords.latitude);
-//       useLon(sucess.coords.longitude);
-//     },
-//     err => {
-//       return console.log('err :>> ', err);
-//     },
-//   );
-// };
-// GetPosition();
 
 const Map = () => {
+  const [position, setPosition] = useState<number[]>();
+  const [hasUserLocation, setHasUserLocation] = useState<boolean>(false);
+
+  const mapIcon = icon({
+    iconUrl: '/assets/petLogoBold.png',
+    iconSize: [60, 60],
+    iconAnchor: [29, 68],
+    popupAnchor: [180, -4],
+  });
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      location => {
+        const { latitude, longitude } = location.coords;
+
+        setHasUserLocation(true);
+        setPosition([latitude, longitude]);
+      },
+      () => {
+        setHasUserLocation(false);
+        setPosition([-13.8013697, -50.9061335]);
+      },
+    );
+  }, []);
+
   return (
-    <MapContainer
-      center={[51.505, -0.09]}
-      zoom={13}
-      scrollWheelZoom={false}
-      style={{ height: '100%', width: '100%' }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {/* <Marker position={[51.505, -0.09]}></Marker> */}
-    </MapContainer>
+    <>
+      {position ? (
+        <MapContainer
+          center={[position[0], position[1]]}
+          zoom={hasUserLocation ? 16 : 5}
+          scrollWheelZoom={true}
+          style={{ height: '100%', width: '100%' }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[position[0], position[1]]} icon={mapIcon}>
+            <Popup
+              closeButton={false}
+              minWidth={240}
+              maxWidth={240}
+              className="map-popup"
+            >
+              Juquinha
+            </Popup>
+          </Marker>
+        </MapContainer>
+      ) : (
+        ''
+      )}
+    </>
   );
 };
 
